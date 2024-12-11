9701c50b6a2d59da7b624c22f45c185f45651480
EmmanuelAVT9 <alexsoge@gmail.com>
2024-12-11 13:52:16 -0600

    feat> Added Cart functionality and updated ProductCard to include addToCart event

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-//import products from "@/data/products.json";
 import { useProductStore } from "@/stores/ProductStore";
+import { useCartStore } from "@/stores/CartStore";
+
 
 const productStore = useProductStore();
+const cartStore = useCartStore();
 productStore.fill();
+const addToCart = (count, product) => {
+  count = parseInt(count)
+  for (let index = 0; index < count; index++) {
+    cartStore.addProduct(product);
+
+  }
+}
+
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
+        @addToCart="cartStore.items.push(product)" />
     </ul>
   </div>
 </template>
